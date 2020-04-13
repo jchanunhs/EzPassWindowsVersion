b7eb114b106728f97633823c33bb3f0a80e6d40c
jchanunhs <53135155+jchanunhs@users.noreply.github.com>
2020-04-13 19:47:03 -0400

    Update Account.java

--- EzPassApplication/src/ezpassapplication/Account.java
+++ EzPassApplication/src/ezpassapplication/Account.java
@@ -60,6 +60,36 @@ public class Account {
         }
         return Name;
     }
+    
+      public String getCustomerID() {
+        String Name = "";
+        try {
+            DBConnection ToDB = new DBConnection();
+            Connection DBConn = ToDB.openConn();
+            Statement Stmt = DBConn.createStatement();
+            String SQL_Command = "SELECT * FROM Account WHERE Username ='" + Username + "'AND Password ='" + Password + "'"; //SQL query command
+            ResultSet Rslt = Stmt.executeQuery(SQL_Command); // Execute
+            Rslt.next();
+            Name = Rslt.getString("CustomerID"); //Fetch name row
+            Stmt.close();
+            ToDB.closeConn();
+        } catch (java.sql.SQLException e) {
+
+            System.out.println("SQLException: " + e);
+            while (e != null) {
+                System.out.println("SQLState: " + e.getSQLState());
+                System.out.println("Message: " + e.getMessage());
+                System.out.println("Vendor: " + e.getErrorCode());
+                e = e.getNextException();
+                System.out.println("");
+            }
+        } catch (java.lang.Exception e) {
+
+            System.out.println("Exception: " + e);
+            e.printStackTrace();
+        }
+        return Name;
+    }
 
     public boolean checkValid(String UName, String OldPass) {
         boolean done = false;
