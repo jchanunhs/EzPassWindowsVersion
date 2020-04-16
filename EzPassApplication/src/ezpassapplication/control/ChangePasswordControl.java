f26da97f41e94d895ca1ba921b239885d83ee1f9
jchanunhs <53135155+jchanunhs@users.noreply.github.com>
2020-04-15 21:25:49 -0400

    Optimized change password mehods

--- EzPassApplication/src/ezpassapplication/control/ChangePasswordControl.java
+++ EzPassApplication/src/ezpassapplication/control/ChangePasswordControl.java
@@ -6,14 +6,10 @@ import javax.swing.JOptionPane;
 public class ChangePasswordControl {
 
     public ChangePasswordControl(String UName, String CID, String OldPass, String NewPass) {
-        Account acct = new Account(CID); //delcare account object
-
-        //Check if the user/pass is correct
-        if (acct.checkValid(UName, OldPass)) {
-            //if account is valid, then change password
-            if (acct.changePassword(NewPass)) {
-                JOptionPane.showMessageDialog(null, "Change password is successful!", "Confirmation", JOptionPane.INFORMATION_MESSAGE);
-            }
+        Account acct = new Account(UName, OldPass); //delcare account object with old password
+        //if account is valid, then change password
+        if (acct.changePassword(NewPass)) {
+            JOptionPane.showMessageDialog(null, "Change password is successful!", "Confirmation", JOptionPane.INFORMATION_MESSAGE);
         } else {
             JOptionPane.showMessageDialog(null, "Change password failed! Old Password is incorrect!", "Confirmation", JOptionPane.ERROR_MESSAGE);
         }
--- EzPassApplication/src/ezpassapplication/model/Account.java
+++ EzPassApplication/src/ezpassapplication/model/Account.java
@@ -20,12 +20,7 @@ public class Account {
         Password = PassW;
 
     }
-    
-    //change account information based on CID
-    public Account(String CID) {
-        CustomerID = CID;
-    }
-    
+   
     //default constructor
     public Account() {
 
@@ -61,7 +56,7 @@ public class Account {
         return Name;
     }
     
-      public String getCustomerID() {
+    public String getCustomerID() {
         String Name = "";
         try {
             DBConnection ToDB = new DBConnection();
@@ -89,44 +84,6 @@ public class Account {
             e.printStackTrace();
         }
         return Name;
-    }
-
-    public boolean checkValid(String UName, String OldPass) {
-        boolean done = false;
-        String DBPass = ""; //PW fetched by DB
-        String DBUName = ""; //UName fetched by DB
-        try {
-            DBConnection ToDB = new DBConnection(); //Have a connection to the DB
-            Connection DBConn = ToDB.openConn();
-            Statement Stmt = DBConn.createStatement();
-            String SQL_Command = "SELECT * FROM Account WHERE CustomerID ='" + CustomerID + "'"; //SQL query command
-            ResultSet Rslt = Stmt.executeQuery(SQL_Command); //check if the customerid exist
-            if (Rslt.next()) {
-                DBPass = Rslt.getString("Password"); 
-                DBUName = Rslt.getString("Username");
-                Stmt.close();
-                ToDB.closeConn();
-
-            }
-            if (OldPass.equals(DBPass) && UName.equals(DBUName)) { //If user name and password matches, then return true
-                done = true;
-            }
-        } catch (java.sql.SQLException e) {
-            done = false;
-            System.out.println("SQLException: " + e);
-            while (e != null) {
-                System.out.println("SQLState: " + e.getSQLState());
-                System.out.println("Message: " + e.getMessage());
-                System.out.println("Vendor: " + e.getErrorCode());
-                e = e.getNextException();
-                System.out.println("");
-            }
-        } catch (java.lang.Exception e) {
-            done = false;
-            System.out.println("Exception: " + e);
-            e.printStackTrace();
-        }
-        return done;
     }
 
     public boolean signUp() {
@@ -195,17 +152,18 @@ public class Account {
         }
         return done;
     }
-
+    
+        
     public boolean changePassword(String NewPassword) {
         boolean done = false;
         try {
             DBConnection ToDB = new DBConnection(); //Have a connection to the DB
             Connection DBConn = ToDB.openConn();
             Statement Stmt = DBConn.createStatement();
-            String SQL_Command = "SELECT * FROM Account WHERE CustomerID ='" + CustomerID + "'"; //Check if customer id exist
+            String SQL_Command = "SELECT * FROM Account WHERE Username ='" + Username + "' AND Password ='" + Password + "'"; //Check if username and password are correct 
             ResultSet Rslt = Stmt.executeQuery(SQL_Command); 
-            if (Rslt.next()) {
-                SQL_Command = "UPDATE Account SET Password='" + NewPassword + "' WHERE CustomerID ='" + CustomerID + "'"; //update password to new password
+            if (Rslt.next()) { // if username and password exist in db, that means user entered old pass correctly and we can change password
+                SQL_Command = "UPDATE Account SET Password='" + NewPassword + "' WHERE Username ='" + Username + "'"; //update password to new password
                 Stmt.executeUpdate(SQL_Command);
                 Stmt.close();
                 ToDB.closeConn();
