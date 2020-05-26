ec8d54b01b710ad3171e5215cdbb132d2b7372f1
jchanunhs <53135155+jchanunhs@users.noreply.github.com>
2020-05-26 13:16:03 -0400

    Check if tag code is invalid

--- EzPassApplication/src/ezpassapplication/control/RemoveTagControl.java
+++ EzPassApplication/src/ezpassapplication/control/RemoveTagControl.java
@@ -9,7 +9,7 @@ import javax.swing.SwingUtilities;
 
 public class RemoveTagControl {
 
-public RemoveTagControl(ActionEvent evt, String TC, String CID) {
+    public RemoveTagControl(ActionEvent evt, String TC, String CID) {
         EzTag tag = new EzTag(TC, CID); //ez tag object contains the tag code and customer ID
 
         //Remove tag and check if tag belongs to customer
@@ -19,8 +19,12 @@ public RemoveTagControl(ActionEvent evt, String TC, String CID) {
             JComponent component = (JComponent) evt.getSource();
             Window win = SwingUtilities.getWindowAncestor(component);
             win.dispose();
-        } else {
-            JOptionPane.showMessageDialog(null, "Remove Tag failed!", "Confirmation", JOptionPane.ERROR_MESSAGE);
+        } else if (!tag.checkTag()) {
+            JOptionPane.showMessageDialog(null, "Error: The tag code you entered is invalid", "Confirmation", JOptionPane.ERROR_MESSAGE);
+        } else { //Remove tag fails if user already used it to pay tolls or tag is currently associated with a vehicle
+            JOptionPane.showMessageDialog(null, "Error: Unable to remove tag code. This error occurs when: \n"
+                    + "1. Tag code was used to pay toll\n"
+                    + "2. Tag code is linked to a vehicle: you can remove vehicle associated with tag code and try again\n", "Confirmation", JOptionPane.ERROR_MESSAGE);
         }
     }
 }
