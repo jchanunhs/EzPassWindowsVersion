d992022d7a2e4adace045ec27a974c111a8d7f2d
jchanunhs <53135155+jchanunhs@users.noreply.github.com>
2020-05-25 22:40:52 -0400

    removed redundancy
    - RemoveTagControl no longer need to check tag. if the CID and Tag Code doesn't match, removeTag will return false

--- EzPassApplication/src/ezpassapplication/control/RemoveTagControl.java
+++ EzPassApplication/src/ezpassapplication/control/RemoveTagControl.java
@@ -14,7 +14,7 @@ public RemoveTagControl(ActionEvent evt, String TC, String CID) {
 
         //Remove tag and check if tag belongs to customer
         //Note: tag will not remove unless tag code cid = current customer cid, aka owner owns this tag code
-        if (tag.checkTag() && tag.removeTag()) {
+        if (tag.removeTag()) {
             JOptionPane.showMessageDialog(null, "Remove Tag is successful!", "Confirmation", JOptionPane.INFORMATION_MESSAGE);
             JComponent component = (JComponent) evt.getSource();
             Window win = SwingUtilities.getWindowAncestor(component);
