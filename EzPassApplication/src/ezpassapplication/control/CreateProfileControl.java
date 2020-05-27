7898801071f5a8539bb5cc7e96a9f41427f3a3bc
jchanunhs <53135155+jchanunhs@users.noreply.github.com>
2020-05-27 18:45:11 -0400

    removed Bal
    Balance is already a float value

--- EzPassApplication/src/ezpassapplication/control/CreateProfileControl.java
+++ EzPassApplication/src/ezpassapplication/control/CreateProfileControl.java
@@ -10,8 +10,7 @@ import javax.swing.SwingUtilities;
 public class CreateProfileControl {
 
     public CreateProfileControl(ActionEvent evt, String Name, String Street, String City, String State, String Zip, String Phone, String Email, float Balance, String UName) {
-        float Bal = Float.parseFloat(String.valueOf(Balance));
-        Customer cus = new Customer(Name, Street, City, State, Zip, Phone, Email, Bal, UName); //declare customer object with information
+        Customer cus = new Customer(Name, Street, City, State, Zip, Phone, Email, Balance, UName); //declare customer object with information
         if (Name.equals("") || Street.equals("") || City.equals("") || State.equals("") || Zip.equals("") || Phone.equals("") || Email.equals("")) {
             JOptionPane.showMessageDialog(null, "Create profile failed! Please fill out all information!", "Confirmation", JOptionPane.ERROR_MESSAGE);
         } else if (cus.createProfile()) { //create the profile 
